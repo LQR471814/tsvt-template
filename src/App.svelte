e502f37505606fcc629062f93edca72e73b35d3d
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2022-07-03 21:36:45 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<main class="flex h-full w-full">
+    <h1 class="font-semibold inline m-auto">Hello world!</h1>
+</main>
+
+<style>
+    :global(body) {
+        margin: 0;
+        height: 100vh;
+    }
+</style>
